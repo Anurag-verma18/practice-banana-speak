c2a6b4186db3bde8e38de50ee989946d0e6f2ce9
Anurag-verma18 <anuragkrverma18@gmail.com>
2023-07-28 17:42:19 +0530

    translation done

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#myinput');
 var txtOutput = document.querySelector('#myoutput');
 
-function clickHandler() {
-    txtOutput.innerText = txtInput.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
 }
-btnTranslate.addEventListener('click', clickHandler)
+
+function clickHandler() {
+    var inputText = txtInput.value;
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(error => {
+            console.log("error occured", error);
+            alert("something wrong with server");
+        })
+}
+btnTranslate.addEventListener('click', clickHandler);
 
